66fa53c6fd95a92fd48a35b3fe0a8c5079fe2ef5
DonizeteVida <donizetevida1998@outlook.com>
2026-06-14 04:21:06 +0200

    Add yoga sample

--- src/main.cpp
+++ src/main.cpp
@@ -5,29 +5,65 @@
 #include <SFML/OpenGL.hpp>
 #include <yoga/Yoga.h>
 
+void calculateShape(YGNodeRef &node, sf::RectangleShape &shape)
+{
+	const auto left = YGNodeLayoutGetLeft(node);
+	const auto top = YGNodeLayoutGetTop(node);
+	const auto width = YGNodeLayoutGetWidth(node);
+	const auto height = YGNodeLayoutGetHeight(node);
+
+	shape.setPosition({left, top});
+	shape.setSize({width, height});
+}
+
 int main()
 {
 	const sf::Clock clock;
 	sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "SFML works!");
-	auto size = window.getSize();
-	std::cout << "Window size: " << size.x << "x" << size.y << std::endl;
+	sf::RectangleShape shape;
+
+	YGNodeRef root = YGNodeNew();
+	YGNodeStyleSetFlexDirection(root, YGFlexDirectionRow);
+
+	YGNodeRef child0 = YGNodeNew();
+	YGNodeStyleSetFlexGrow(child0, 1.0f);
+	YGNodeStyleSetFlexBasis(child0, 0.0f);
+	YGNodeInsertChild(root, child0, 0);
+
+	YGNodeRef child1 = YGNodeNew();
+	YGNodeStyleSetFlexGrow(child1, 1.0f);
+	YGNodeStyleSetFlexBasis(child1, 0.0f);
+	YGNodeInsertChild(root, child1, 1);
 
 	while (window.isOpen())
 	{
 		while (const std::optional event = window.pollEvent())
 		{
 			if (event->is<sf::Event::Closed>())
+			{
 				window.close();
+			}
+			else if (const auto &resize = event->getIf<sf::Event::Resized>())
+			{
+				const auto [width, height] = resize->size;
+
+				std::cout << "Resized: " << width << "x" << height << std::endl;
+
+				YGNodeStyleSetWidth(root, width);
+				YGNodeStyleSetHeight(root, height);
+				YGNodeCalculateLayout(root, width, height, YGDirectionLTR);
+			}
 		}
 
-		window.clear();
+		window.clear(sf::Color::Black);
 
-		glClearColor(
-			sin(clock.getElapsedTime().asSeconds()),
-			cos(clock.getElapsedTime().asSeconds()),
-			tan(clock.getElapsedTime().asSeconds()),
-			1);
-		glClear(GL_COLOR_BUFFER_BIT);
+		calculateShape(child0, shape);
+		shape.setFillColor(sf::Color::Red);
+		window.draw(shape);
+
+		calculateShape(child1, shape);
+		shape.setFillColor(sf::Color::Green);
+		window.draw(shape);
 
 		window.display();
 	}
