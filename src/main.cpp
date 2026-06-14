ddb4c8ad41514856afbdd0abf402e543db80ef73
DonizeteVida <donizetevida1998@outlook.com>
2026-06-14 03:22:39 +0200

    Call an OpenGL function

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Main.hpp>
+#include <SFML/OpenGL.hpp>
 
 int main()
 {
+	const sf::Clock clock;
 	sf::RenderWindow window( sf::VideoMode( { 200, 200 } ), "SFML works!" );
 	sf::CircleShape shape( 100.f );
 	shape.setFillColor( sf::Color::Green );
@@ -16,7 +18,14 @@ int main()
 		}
 
 		window.clear();
-		window.draw( shape );
+
+        glClearColor(
+            sin(clock.getElapsedTime().asSeconds()),
+            cos(clock.getElapsedTime().asSeconds()),
+            tan(clock.getElapsedTime().asSeconds()),
+            1);
+        glClear(GL_COLOR_BUFFER_BIT);
+
 		window.display();
 	}
 }
