4c394dc8d7135800965699f980b98484aabfdc86
DonizeteVida <donizetevida1998@outlook.com>
2026-06-14 03:53:40 +0200

    Add yoga dependency

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,33 @@
+#include <iostream>
+
 #include <SFML/Graphics.hpp>
 #include <SFML/Main.hpp>
 #include <SFML/OpenGL.hpp>
+#include <yoga/Yoga.h>
 
 int main()
 {
 	const sf::Clock clock;
-	sf::RenderWindow window( sf::VideoMode( { 200, 200 } ), "SFML works!" );
-	sf::CircleShape shape( 100.f );
-	shape.setFillColor( sf::Color::Green );
+	sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "SFML works!");
+	auto size = window.getSize();
+	std::cout << "Window size: " << size.x << "x" << size.y << std::endl;
 
-	while ( window.isOpen() )
+	while (window.isOpen())
 	{
-		while ( const std::optional event = window.pollEvent() )
+		while (const std::optional event = window.pollEvent())
 		{
-			if ( event->is<sf::Event::Closed>() )
+			if (event->is<sf::Event::Closed>())
 				window.close();
 		}
 
 		window.clear();
 
-        glClearColor(
-            sin(clock.getElapsedTime().asSeconds()),
-            cos(clock.getElapsedTime().asSeconds()),
-            tan(clock.getElapsedTime().asSeconds()),
-            1);
-        glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(
+			sin(clock.getElapsedTime().asSeconds()),
+			cos(clock.getElapsedTime().asSeconds()),
+			tan(clock.getElapsedTime().asSeconds()),
+			1);
+		glClear(GL_COLOR_BUFFER_BIT);
 
 		window.display();
 	}
